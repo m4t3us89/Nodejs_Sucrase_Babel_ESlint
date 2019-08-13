79c06cf984a4910b2f0c5f32230ad8d0740ae1cc
Allisson Mateus <allisson.mateus@crptecnologia.com.br>
2019-08-13 14:07:13 -0300

    Formato Function

--- src/server.js
+++ src/server.js
@@ -5,6 +5,32 @@ import cors from 'cors'
 import routesRoot from './app/routesRoot'
 import * as Sentry from '@sentry/node'
 
+function init () {
+  const app = express()
+  dotenv.config()
+
+  Sentry.init({
+    dsn: 'https://dc21ad777e7141c1a77919f583dd0709@sentry.io/1529577'
+  })
+
+  app.use(Sentry.Handlers.requestHandler())
+
+  app.use(morgan('dev'))
+  app.use(cors())
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+
+  routesRoot.forEach(router => router(app))
+
+  app.use(Sentry.Handlers.errorHandler())
+
+  app.listen(process.ENV || 3333, () =>
+    console.log(`On Server ${process.ENV || 3333}`)
+  )
+}
+init()
+
+/*
 class Server {
   constructor () {
     this.express = express()
@@ -57,25 +83,4 @@ class Server {
 
 const app = new Server()
 app.init()
-
-/* const app = express()
-dotenv.config()
-
-Sentry.init({
-  dsn: 'https://dc21ad777e7141c1a77919f583dd0709@sentry.io/1529577'
-})
-
-app.use(Sentry.Handlers.requestHandler())
-
-app.use(morgan('dev'))
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-routesRoot.forEach(router => router(app))
-
-app.use(Sentry.Handlers.errorHandler())
-
-app.listen(process.ENV || 3333, () =>
-  console.log(`On Server ${process.ENV || 3333}`)
-) */
+*/
