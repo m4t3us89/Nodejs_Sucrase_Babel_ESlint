dc56f28b87ca6b27ea6c7ca29b761d4063e223f7
Allisson Mateus <allisson.mateus@crptecnologia.com.br>
2019-08-13 11:31:55 -0300

    Formato Classe

--- src/server.js
+++ src/server.js
@@ -5,7 +5,60 @@ import cors from 'cors'
 import routesRoot from './app/routesRoot'
 import * as Sentry from '@sentry/node'
 
-const app = express()
+class Server {
+  constructor () {
+    this.express = express()
+    this.routesRoot = routesRoot
+    this.sentry = Sentry
+    this.dotenv = dotenv
+  }
+
+  init () {
+    this.dotenvInit()
+    this.sentryInit()
+    this.middlewaresBeforeRoutes()
+    this.routes()
+    this.middlewaresAfterRoutes()
+    this.listenServer()
+  }
+
+  dotenvInit () {
+    this.dotenv.config()
+  }
+
+  sentryInit () {
+    this.sentry.init({
+      dsn: 'https://dc21ad777e7141c1a77919f583dd0709@sentry.io/1529577'
+    })
+  }
+
+  middlewaresBeforeRoutes () {
+    this.express.use(this.sentry.Handlers.requestHandler())
+    this.express.use(morgan('dev'))
+    this.express.use(cors())
+    this.express.use(express.json())
+    this.express.use(express.urlencoded({ extended: true }))
+  }
+
+  middlewaresAfterRoutes () {
+    this.express.use(this.sentry.Handlers.errorHandler())
+  }
+
+  routes () {
+    this.routesRoot.forEach(router => router(this.express))
+  }
+
+  listenServer () {
+    this.express.listen(process.ENV || 3333, () =>
+      console.log(`On Server ${process.ENV || 3333}`)
+    )
+  }
+}
+
+const app = new Server()
+app.init()
+
+/* const app = express()
 dotenv.config()
 
 Sentry.init({
@@ -25,4 +78,4 @@ app.use(Sentry.Handlers.errorHandler())
 
 app.listen(process.ENV || 3333, () =>
   console.log(`On Server ${process.ENV || 3333}`)
-)
+) */
