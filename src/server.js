496daa0f96e598ee0f711db4c8260dece97208ed
Allisson Mateus <allisson.mateus@crptecnologia.com.br>
2019-08-13 10:47:32 -0300

    Inclusão do Sentry

--- src/server.js
+++ src/server.js
@@ -3,9 +3,16 @@ import dotenv from 'dotenv'
 import morgan from 'morgan'
 import cors from 'cors'
 import routesRoot from './app/routesRoot'
+import * as Sentry from '@sentry/node'
 
 const app = express()
 dotenv.config()
+
+Sentry.init({
+  dsn: 'https://dc21ad777e7141c1a77919f583dd0709@sentry.io/1529577'
+})
+
+app.use(Sentry.Handlers.requestHandler())
 
 app.use(morgan('dev'))
 app.use(cors())
@@ -14,6 +21,8 @@ app.use(express.urlencoded({ extended: true }))
 
 routesRoot.forEach(router => router(app))
 
-app.listen(process.ENV || 8000, () => {
-  console.log(`On Server ${process.ENV || 8000}`)
-})
+app.use(Sentry.Handlers.errorHandler())
+
+app.listen(process.ENV || 3333, () =>
+  console.log(`On Server ${process.ENV || 3333}`)
+)
