55844dfbaf8dd1633c78ec6707f46caff93f9d54
Allisson Mateus <allisson.mateus@crptecnologia.com.br>
2019-08-15 18:22:31 -0300

    Listando Users

--- src/app/todo/index.js
+++ src/app/todo/index.js
@@ -1,13 +1,16 @@
 import api from '../../services/api'
+import Todos from '../models'
 
 class Todo {
   async list (req, res) {
-    try {
-      const { data: todos } = await api.get('todos')
-      return res.status(201).send(todos)
-    } catch (err) {
-      return res.status(400)
-    }
+    // try {
+    // const { data: todos } = await api.get('todos')
+    const usersdfa = await Todos.User.findAll({})
+    console.log('sadfasdfasdfasdfasd')
+    return res.status(201).send('usersdfa')
+    // } catch (err) {
+    //   return res.status(400)
+    // }
   }
 }
 
