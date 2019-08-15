cb0f17839065e1cdd1fdc101cd4df1a4b6d27a5e
Allisson Mateus <allisson.mateus@crptecnologia.com.br>
2019-08-15 17:58:58 -0300

    Sequelize ORM

---
+++ src/config/database.js
@@ -0,0 +1,8 @@
+module.exports = {
+  username: 'root',
+  password: 'password',
+  database: 'crud_sequelize',
+  host: '127.0.0.1',
+  dialect: 'mysql',
+  port: ''
+}
--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -4,7 +4,7 @@ export default async (req, res, next) => {
   try {
     const { token } = req.headers
     await jsonwebtoken.verify(token, 'shhh')
-    return res.next()
+    return next()
   } catch (err) {
     return res.status(401).send({
       message: 'Acesso Negado'
